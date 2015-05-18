a0a2e3c039fb6a5c5c306bc441467d894a673a6a
David Zheng <dzd123@mit.edu>
2015-05-18 12:31:14 -0400

    Display of pieces and click events now works

--- draw.py
+++ draw.py
@@ -45,38 +45,46 @@ class BoardGuiTk(tk.Frame):
 
 
     def click(self, event):
-        pass
-
-    def update_piece(self, i, j): # updates model
-        if self.board.place_piece(i, j):
-            refresh()
+        margin = self.square_size / 2
+        marginx = (self.width - (self.rows - 1) * self.square_size) / 2
+        marginy = (self.height - (self.columns - 1) * self.square_size) / 2
+        radius = self.square_size / 4 
         
-    def addpiece(self, name, image, row=0, column=0): # draws piece
-        '''Add a piece to the playing board'''
-        x0 = (column * self.square_size) + int(self.square_size/2)
-        y0 = (row * self.square_size) + int(self.square_size/2)
-        self.canvas.create_image(x0,y0, image=image, tags=(name, "piece"), anchor="c")
-
+        near_i = (event.y - marginy)/float(self.square_size)
+        near_j = (event.x - marginx)/float(self.square_size)
+        round_i = int(near_i) if near_i - int(near_i) < .5 else int(near_i) + 1
+        round_j = int(near_j) if near_j - int(near_j) < .5 else int(near_j) + 1
+        if abs(near_i - round_i) > radius/float(self.square_size) or abs(near_j - round_j) > radius/float(self.square_size):
+            return
+        else:
+            self.board.place_piece(round_i,round_j)
+        self.refresh()
+        
     def refresh(self, event={}):
 
         '''Redraw the board'''
         if event:
             xsize = int((event.width) / (self.columns))
             ysize = int((event.height-30) / (self.rows))
+            self.width = event.width
+            self.height = event.height
             self.square_size = min(xsize, ysize)
-
-
+        if self.board.turn == 1:
+            self.label_status['text'] = "   White's turn  "
+        else:
+            self.label_status['text'] = "   Black's turn  "
         self.canvas.delete("square")
         self.canvas.delete("bg")
         margin = self.square_size / 2
-        marginx = (event.width - (self.rows - 1) * self.square_size) / 2
-        marginy = (event.height - (self.columns - 1) * self.square_size) / 2   
+        marginx = (self.width - (self.rows - 1) * self.square_size) / 2
+        marginy = (self.height - (self.columns - 1) * self.square_size) / 2   
         self.canvas.create_rectangle(marginx - margin, marginy - margin, marginx + (self.rows-1)*self.square_size + margin, marginy + (self.columns-1)*self.square_size + margin,
                                      fill=self.bgcolor, width=0, tags="bg")     
         for row in range(self.rows - 1):
             for col in range(self.columns - 1):
                 x1 = (col * self.square_size) + marginx
                 y1 = (row * self.square_size) + marginy
+
                 x2 = x1 + self.square_size
                 y2 = y1 + self.square_size
                 self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", tags="square", width=2)
@@ -88,32 +96,42 @@ class BoardGuiTk(tk.Frame):
     
     def draw_pieces(self):
         self.canvas.delete("piece")
-        for x in range(self.rows):
-            for y in range(self.columns):
-                if self.board.board[x][y] == 0:
+        radius = self.square_size/3
+        margin = self.square_size / 2
+        marginx = (self.width - (self.rows - 1) * self.square_size) / 2
+        marginy = (self.height - (self.columns - 1) * self.square_size) / 2   
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if self.board.board[i][j] == 0:
                     continue
-                piece = self.board.board[x][y]
-                filename = "img/white.png" if piece == 1 else "img/black.png"
-                piecename = "white" if piece == 1 else "black"
-                piecename += " " + str(x) + " " + str(y)
+                # piece = self.board.board[x][y]
+                # filename = "img/white.png" if piece == 1 else "img/black.png"
+                # piecename = "white" if piece == 1 else "black"
+                # piecename += " " + str(x) + " " + str(y)
 
-                if(filename not in self.icons):
-                    self.icons[filename] = ImageTk.PhotoImage(file=filename, width=32, height=32)
-                self.addpiece(piecename, self.icons[filename], x, y)
+                # if(filename not in self.icons):
+                #     self.icons[filename] = ImageTk.PhotoImage(file=filename, width=32, height=32)
+                # self.addpiece(piecename, self.icons[filename], x, y)
+                if self.board.board[i][j] == 1:
+                    self.canvas.create_oval(marginx + j*self.square_size - radius, marginy + i*self.square_size - radius, \
+                 marginx + j*self.square_size + radius, marginy + i*self.square_size + radius, tags = ("piece"), \
+                 fill = "white", outline = "black")
+                else: 
+                    self.canvas.create_oval(marginx + j*self.square_size - radius, marginy + i*self.square_size - radius, \
+                 marginx + j*self.square_size + radius, marginy + i*self.square_size + radius, tags = ("piece"), \
+                 fill = "black")
   
     def reset(self):
-        self.chessboard.load(board.FEN_STARTING)
         self.refresh()
-        self.draw_pieces()
+        # self.draw_pieces()
         # self.refresh()
 
 def display(board):
     root = tk.Tk()
-    root.title("Simple Python Chess")
+    root.title("Python Go")
 
     gui = BoardGuiTk(root, board)
     gui.pack(side="top", fill="both", expand="true", padx=4, pady=4)
-    gui.draw_pieces()
 
     #root.resizable(0,0)
     root.mainloop()
@@ -121,5 +139,5 @@ def display(board):
 if __name__ == "__main__":
     size = 19
     board = board.Board(size)
-    board.place_piece(2,2)
+    board.place_piece(0,0)
     display(board)
