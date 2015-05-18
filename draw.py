3ee87e6a96e5b4ed69261c1ca4b1f7be6b924568
stevenhao <stevenhao@gmail.com>
2015-05-18 15:32:59 -0400

    code clean up, implement hover
    hover:
      'shadow' piece displays under mouse
      actual pieces have gray border
    
    added the _ prefix to internal methods
    more modularity in drawing code
    established convention:
      (i, j) denotes game-coordinates,
      (x, y) denotes screen coordinates
    cleaned layout code

--- draw.py
+++ draw.py
@@ -1,6 +1,5 @@
 import Tkinter as tk
-import board
-from PIL import ImageTk
+from board import Board
 
 class BoardGuiTk(tk.Frame):
     bgcolor = "yellow"
@@ -11,10 +10,11 @@ class BoardGuiTk(tk.Frame):
 
     def __init__(self, parent, board, square_size=64, min_square_size=30):
         self.board = board
-        self.rows = len(board.board)
-        self.columns = len(board.board)
+        self.rows = board.size
+        self.columns = board.size
         self.square_size = square_size
         self.parent = parent
+        self.hover = None
 
         self.icons = {}
 
@@ -26,14 +26,13 @@ class BoardGuiTk(tk.Frame):
         self.canvas = tk.Canvas(self, width=min_canvas_width, height=min_canvas_height, background="grey")
         self.canvas.pack(side="top", fill="both", anchor="c", expand=True)
 
-        self.canvas.bind("<Configure>", self.refresh)
-        self.canvas.bind("<Button-1>", self.click)
+        self.canvas.bind("<Configure>", self._refresh)
+        self.canvas.bind("<Button-1>", self._mouse_click)
+        self.canvas.bind("<Motion>", self._mouse_move)
 
         self.statusbar = tk.Frame(self, height=64)
-        self.button_quit = tk.Button(self.statusbar, text="New", fg="black", command=self.reset)
-        self.button_quit.pack(side=tk.LEFT, in_=self.statusbar)
 
-        self.button_save = tk.Button(self.statusbar, text="Save", fg="black", command=self.reset)
+        self.button_save = tk.Button(self.statusbar, text="Save", fg="black", command=self._refresh)
         self.button_save.pack(side=tk.LEFT, in_=self.statusbar)
 
         self.label_status = tk.Label(self.statusbar, text="   White's turn  ", fg="black")
@@ -44,88 +43,94 @@ class BoardGuiTk(tk.Frame):
         self.statusbar.pack(expand=False, fill="both", side='bottom')
 
 
-    def click(self, event):
-        margin = self.square_size / 2
-        marginx = (self.width - (self.rows - 1) * self.square_size) / 2
-        marginy = (self.height - (self.columns - 1) * self.square_size) / 2
-        radius = self.square_size / 4 
-        
-        near_i = (event.y - marginy)/float(self.square_size)
-        near_j = (event.x - marginx)/float(self.square_size)
-        round_i = int(near_i) if near_i - int(near_i) < .5 else int(near_i) + 1
-        round_j = int(near_j) if near_j - int(near_j) < .5 else int(near_j) + 1
-        if abs(near_i - round_i) > radius/float(self.square_size) or abs(near_j - round_j) > radius/float(self.square_size):
-            return
-        else:
-            self.board.place_piece(round_i,round_j)
-        self.refresh()
-        
-    def refresh(self, event={}):
+    def _screenLoc(self, i, j):
+        x = j*self.square_size + self.marginx
+        y = i*self.square_size + self.marginy
+        return x, y
 
-        '''Redraw the board'''
+    def _gridLoc(self, x, y):
+        i = float(y - self.marginy)/self.square_size
+        j = float(x - self.marginx)/self.square_size
+        return int(i + .5), int(j + .5)
+
+    def _mouse_move(self, event):
+        i, j = self._gridLoc(event.x, event.y)
+
+        if self.board.in_bounds(i, j) and not self.board.get(i, j):
+            self.hover = i, j
+        else:
+            self.hover = None
+        self._draw_hover()
+
+    def _mouse_click(self, event):
+        i, j = self._gridLoc(event.x, event.y)
+
+        self.board.place_piece(i, j)
+        self._draw_pieces()
+        
+    def _refresh(self, event={}):
         if event:
             xsize = int((event.width) / (self.columns))
             ysize = int((event.height-30) / (self.rows))
             self.width = event.width
             self.height = event.height
             self.square_size = min(xsize, ysize)
+            self.marginx = (self.width - (self.rows - 1) * self.square_size) / 2
+            self.marginy = (self.height - (self.columns - 1) * self.square_size) / 2  
+        
         if self.board.turn == 1:
             self.label_status['text'] = "   White's turn  "
         else:
             self.label_status['text'] = "   Black's turn  "
-        self.canvas.delete("square")
-        self.canvas.delete("bg")
-        margin = self.square_size / 2
-        marginx = (self.width - (self.rows - 1) * self.square_size) / 2
-        marginy = (self.height - (self.columns - 1) * self.square_size) / 2   
-        self.canvas.create_rectangle(marginx - margin, marginy - margin, marginx + (self.rows-1)*self.square_size + margin, marginy + (self.columns-1)*self.square_size + margin,
-                                     fill=self.bgcolor, width=0, tags="bg")     
-        for row in range(self.rows - 1):
-            for col in range(self.columns - 1):
-                x1 = (col * self.square_size) + marginx
-                y1 = (row * self.square_size) + marginy
 
-                x2 = x1 + self.square_size
-                y2 = y1 + self.square_size
-                self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", tags="square", width=2)
-
-        self.draw_pieces()
-        self.canvas.tag_raise("piece")
-        self.canvas.tag_lower("square")
-        self.canvas.tag_lower("bg")
+        self._draw_board()
+        self._draw_hover()
+        self._draw_pieces()
     
-    def draw_pieces(self):
+    def _draw_board(self):
+        self.canvas.delete("line")
+        self.canvas.delete("background") 
+
+        margin = self.square_size / 2
+        x0, y0 = self._screenLoc(0, 0)
+        x1, y1 = self._screenLoc(self.rows - 1, self.columns - 1)
+        self.canvas.create_rectangle(x0 - margin, y0 - margin, x1 + margin, y1 + margin,
+                                     fill=self.bgcolor, width=0, tags="background")     
+
+        for i in range(self.rows - 1):
+            for j in range(self.columns - 1):
+                x, y = self._screenLoc(i, j)
+                self.canvas.create_rectangle(x, y, x + self.square_size, y + self.square_size, 
+                                             outline="black", tags="line", width=2)      
+        self.canvas.tag_lower("line")
+        self.canvas.tag_lower("background")
+
+
+    def _draw_hover(self):
+        self.canvas.delete("hover")
+        if self.hover:
+            radius = self.square_size/3
+            i, j = self.hover
+            x, y = self._screenLoc(i, j)
+            color = "white" if self.board.turn == 1 else "black"
+            self.canvas.create_oval(x - radius, y - radius, x + radius, y + radius,
+                            tags=("hover"), fill=color)
+        self.canvas.tag_raise("hover")
+        self.canvas.tag_raise("piece")
+
+    def _draw_pieces(self):
         self.canvas.delete("piece")
         radius = self.square_size/3
-        margin = self.square_size / 2
-        marginx = (self.width - (self.rows - 1) * self.square_size) / 2
-        marginy = (self.height - (self.columns - 1) * self.square_size) / 2   
         for i in range(self.rows):
             for j in range(self.columns):
-                if self.board.board[i][j] == 0:
-                    continue
-                # piece = self.board.board[x][y]
-                # filename = "img/white.png" if piece == 1 else "img/black.png"
-                # piecename = "white" if piece == 1 else "black"
-                # piecename += " " + str(x) + " " + str(y)
-
-                # if(filename not in self.icons):
-                #     self.icons[filename] = ImageTk.PhotoImage(file=filename, width=32, height=32)
-                # self.addpiece(piecename, self.icons[filename], x, y)
-                if self.board.board[i][j] == 1:
-                    self.canvas.create_oval(marginx + j*self.square_size - radius, marginy + i*self.square_size - radius, \
-                 marginx + j*self.square_size + radius, marginy + i*self.square_size + radius, tags = ("piece"), \
-                 fill = "white", outline = "black")
-                else: 
-                    self.canvas.create_oval(marginx + j*self.square_size - radius, marginy + i*self.square_size - radius, \
-                 marginx + j*self.square_size + radius, marginy + i*self.square_size + radius, tags = ("piece"), \
-                 fill = "black")
+                if self.board.board[i][j]:
+                    x, y = self._screenLoc(i, j)
+                    color = "white" if self.board.get(i, j) == 1 else "black"
+                    outline_color = "gray"
+                    self.canvas.create_oval(x - radius, y - radius, x + radius, y + radius,
+                                            tags=("piece"), fill=color, outline=outline_color, width=2)
+        self.canvas.tag_raise("piece")
   
-    def reset(self):
-        self.refresh()
-        # self.draw_pieces()
-        # self.refresh()
-
 def display(board):
     root = tk.Tk()
     root.title("Python Go")
@@ -133,10 +138,10 @@ def display(board):
     gui = BoardGuiTk(root, board)
     gui.pack(side="top", fill="both", expand="true", padx=4, pady=4)
 
-    #root.resizable(0,0)
+    # root.resizable(0,0)
     root.mainloop()
 
 if __name__ == "__main__":
     size = 19
-    board = board.Board(size)
+    board = Board(size)
     display(board)
