fa8cfeab7031881a4e0528a6134f2ba1954ed61c
David Zheng <dzd123@mit.edu>
2015-05-19 15:07:59 -0400

    Added superko rule

--- go_board.py
+++ go_board.py
@@ -6,12 +6,38 @@ class Board:
         self.turn = 2
         self.passed_last_turn = False
         self.gameover = False
+        self.prev_states = {}
+
+    @staticmethod
+    def hash_board(board):
+        coeff = 1
+        total = 0
+        for row in board:
+            for cell in row:
+                total += cell*coeff
+                coeff *= 3
+        return total + 1
+
+    def check_state(self, board):
+        hash_value = Board.hash_board(board)
+        return (hash_value, hash_value not in self.prev_states)
+
+    def add_state(self, hash_value):
+        self.prev_states[hash_value] = True
 
     def get(self, i, j):
         return self.board[i][j]
 
+    '''
+        Returns hash_value of new gamestate is valid and unique
+        Returns 0 otherwise
+    '''
     def valid_move(self, i, j):
-        return self.in_bounds(i, j) and not self.get(i, j) and self._check_suicides(i, j)
+        if self.in_bounds(i, j) and not self.get(i, j):
+            hash_value = self._check_suicides_and_ko(i, j)
+            if hash_value:
+                return hash_value
+        return 0
 
     def in_bounds(self, i, j):
         return 0 <= i < self.size and 0 <= j < self.size
@@ -32,8 +58,11 @@ class Board:
             return False
 
         board = self.board
-        if not self.valid_move(i, j):
+        hash_value = self.valid_move(i, j)
+        if not hash_value:
             return False
+
+        self.add_state(hash_value)
 
         board[i][j] = self.turn
 
@@ -49,28 +78,55 @@ class Board:
         else:
             self._elim_pieces_player(1, i, j, True, elim=True)
 
-    def _check_suicides(self, i, j):
+    '''
+        Returns hash_value of new gamestate if gamestate is valid and unique
+        Otherwise, returns 0
+
+        Requires (i,j) to be valid not considering suicides and ko. 
+    '''
+    def _check_suicides_and_ko(self, i, j):
         if self.turn == 1:
             first, second = (1, 2)
         else:
             first, second = (2, 1)
 
         self.board[i][j] = first
-        if not self._elim_pieces_player(second, i, j, True, elim=False): # checks if can eliminate opponent's piece
+        result, hash_value = self._elim_pieces_player(second, i, j, True, elim=False) # checks if can eliminate opponent's piece
+        if result:
             self.board[i][j] = 0
-            return True
-        success = self._elim_pieces_player(first, i, j, False, elim=False) # checks if can eliminate one's own piece
+            if hash_value:
+                return hash_value
+            else:
+                return 0
+            
+        result, hash_value = self._elim_pieces_player(first, i, j, False, elim=False) # checks if can eliminate one's own piece
         self.board[i][j] = 0
-        return success
 
-    def _elim_pieces_player(self, player, i0, j0, check_adj, elim=True):
+        if result and hash_value:
+            return hash_value
+        else:
+            return 0
+
+    '''
+        When elim is True, eliminates "player"'s pieces around i0,j0 if the group has no liberties
+        When elim is False:
+            If is_opponent is True:
+                Returns (True, hash_value) if the state will eliminate opponent's pieces
+                Returns (True, None) if the state is the same as a previous gamestate
+                Returns (False, None) if the state will not eliminate opponent's pieces
+            If is_opponent is False:
+                Returns (True, hash_value) if the state does not eliminate one's own pieces
+                Returns (True, None) if the state is the same as a previous gamestate
+                Returns (False, None) if the state will eliminate one's own pieces
+    '''
+    def _elim_pieces_player(self, player, i0, j0, is_opponent, elim=True):
         board = self.board
         width = len(board)
 
         checked = [[False for i in range(width)] for j in range(width)]
 
         to_eliminate = []
-        check_squares = [(0,1), (0,-1), (-1, 0), (1, 0)] if check_adj else [(0,0)]
+        check_squares = [(0,1), (0,-1), (-1, 0), (1, 0)] if is_opponent else [(0,0)]
         for di,dj in check_squares:
             i,j = (i0+di, j0+dj)
             if not self.in_bounds(i, j) or checked[i][j]:
@@ -82,11 +138,30 @@ class Board:
             newly_checked = []
             if not self._elim_pieces_floodfill(i, j, checked, player, False, newly_checked):
                 to_eliminate.extend(newly_checked)
+        
+        if not elim:
+            if is_opponent:
+                if len(to_eliminate) == 0:
+                    return (False, None)
+            else:
+                if len(to_eliminate) != 0:
+                    return (False, None)
+
+        for i, j in to_eliminate:
+            board[i][j] = 0
+
         if elim:
-            for i, j in to_eliminate:
-                board[i][j] = 0
+            return
+
+        hash_value, is_unique_state = self.check_state(board)
+        for i,j in to_eliminate:
+            board[i][j] = player
+        if is_unique_state:
+            return (True, hash_value)
         else:
-            return len(to_eliminate) == 0
+            return (True, None)
+
+
 
     def _elim_pieces_floodfill(self, i, j, checked, player, has_liberty, newly_checked):
         board = self.board
@@ -117,4 +192,3 @@ class Board:
 
     def _next_turn(self):
         self.turn = 2 if self.turn == 1 else 1
-
